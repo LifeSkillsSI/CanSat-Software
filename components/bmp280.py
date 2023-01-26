67276912f96d1ebeab3f1476837718098ba45600
Jan Przebor <przebot@gmail.com>
2023-01-26 22:45:01 +0100

    Added ground level pressure to BMP280 class

--- components/bmp280.py
+++ components/bmp280.py
@@ -3,8 +3,9 @@ from smbus2 import SMBus
 
 class BMP280():
     # Set i2c to False in order to use BMP280 over spi
-    def __init__(self, i2c = True, i2c_bus = 0):
+    def __init__(self, i2c = True, i2c_bus = 0, gnp = 1008.0):
         self.i2c_enabled = i2c
+        self.gnp = gnp # Ground level pressure
         if i2c:
             self.smbus = SMBus(i2c_bus)
             self._i2cbmp = BMP280I2C(i2c_dev=self.smbus)
@@ -26,7 +27,7 @@ class BMP280():
     
     def get_altitude(self):
         if self.i2c_enabled:
-            return self._i2cbmp.get_altitude()
+            return self._i2cbmp.get_altitude(qnh = self.gnp)
         else:
             pass
         
