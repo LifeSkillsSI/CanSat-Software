2fac713589bd65a17e1bdbb2b0fa4e24fc1b774c
Jan Przebor <przebot@gmail.com>
2023-01-27 21:28:37 +0100

    Formatting changes, requirements.txt, more temperature readings

--- sensor_test.py
+++ sensor_test.py
@@ -6,6 +6,7 @@ from components.icm20948 import ICM20948
 from components.lm35 import LM35
 from components.mcp3008 import MCP3008
 from smbus2 import SMBus
+import psutil
 
 def main_loop():
     global mcp3008, lm35, bmp280
@@ -24,18 +25,25 @@ def main_loop():
         bmp_altitude = bmp280.get_altitude()
         x, y, z = icm20948.read_magnetometer_data()
         ax, ay, az, gx, gy, gz = icm20948.read_accelerometer_gyro_data()
+        try:
+          cpu_temp = psutil.sensors_temperatures()['cpu_thermal'][0].current
+        except:
+          cpu_temp = -1.0
+        icm_temp = icm20948.read_temperature()
 
-        print("""[*] LM35 temperature: %f
+        print("""[*] LM35 temperature: %.2f *C
 [*] BMP280:
-\t- %f *C
-\t- %f hPa
-\t- %f m
+\t- %.2f *C
+\t- %.2f hPa
+\t- %.2f m
 [*] ICM20948:
 \tAccel: %.2f %.2f %.2f
 \tGyro:  %.2f %.2f %.2f
 \tMag:   %.2f %.2f %.2f
-[*] Reading took %f seconds""" % (
-            lm_temp, bmp_temp, bmp_pressure, bmp_altitude, ax, ay, az, gx, gy, gz, x, y, z, time.time()-beginning
+\tDie Temp:  %.2f *C
+[*] CPU temperature: %.2f *C
+[*] Reading took %.4f seconds""" % (
+            lm_temp, bmp_temp, bmp_pressure, bmp_altitude, ax, ay, az, gx, gy, gz, x, y, z, icm_temp, cpu_temp, time.time()-beginning
         ))
         time.sleep(1-(time.time()-beginning))
 
