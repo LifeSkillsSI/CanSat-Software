8f29497ab420d0c07bdcf1640471d20c2da7205e
Jan Przebor <przebot@gmail.com>
2023-01-29 15:32:52 +0100

    Added logging script (for tests) and updated ports

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # CanSat-Software
 This is a repository for LifeSkills CanSat's on-board software.
 To-do: main algorithm loop prototype.
+## Requirements
+- requirements.txt
+- [our pySX127x fork](https://github.com/LifeSkillsSI/pySX127x)
+
 ## Supported components
 - BMP280 (I2C)
 - MCP3008 (SPI)
--- temperature_logging_test.py
+++ temperature_logging_test.py
@@ -1,12 +1,16 @@
 import signal
 import sys
 import time
+import csv
 from components.bmp280 import BMP280
 from components.icm20948 import ICM20948
 from components.lm35 import LM35
 from components.mcp3008 import MCP3008
 from smbus2 import SMBus
 import psutil
+
+TIME = 15*60; # 15 minutes
+table = []
 
 def main_loop():
     global mcp3008, lm35, bmp280
@@ -15,37 +19,28 @@ def main_loop():
     smbus = SMBus(0)
     bmp280 = BMP280(i2c_bus=smbus)
     icm20948 = ICM20948(i2c_bus=smbus)
+    counter = 0
 
-    while True:
+    while counter < TIME:
+        print("[*] Reading sensor data no. %d" % counter)
         beginning = time.time()
 
         lm_temp = lm35.get_temperature()
         bmp_temp = bmp280.get_temperature()
-        bmp_pressure = bmp280.get_pressure()
-        bmp_altitude = bmp280.get_altitude()
-        x, y, z = icm20948.read_magnetometer_data()
-        ax, ay, az, gx, gy, gz = icm20948.read_accelerometer_gyro_data()
         try:
           cpu_temp = psutil.sensors_temperatures()['cpu_thermal'][0].current
         except:
           cpu_temp = -1.0
         icm_temp = icm20948.read_temperature()
 
-        print("""[*] LM35 temperature: %.2f *C
-[*] BMP280:
-\t- %.2f *C
-\t- %.2f hPa
-\t- %.2f m
-[*] ICM20948:
-\tAccel: %.2f %.2f %.2f
-\tGyro:  %.2f %.2f %.2f
-\tMag:   %.2f %.2f %.2f
-\tDie Temp:  %.2f *C
-[*] CPU temperature: %.2f *C
-[*] Reading took %.4f seconds""" % (
-            lm_temp, bmp_temp, bmp_pressure, bmp_altitude, ax, ay, az, gx, gy, gz, x, y, z, icm_temp, cpu_temp, time.time()-beginning
-        ))
+        table.append([int((time.time()-beginning)*10), lm_temp, bmp_temp, cpu_temp, icm_temp])
+
         time.sleep(1-(time.time()-beginning))
+        counter += 1
+
+    with open("data.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(table)
 
 def exit_handler(signal, frame):
   print("[*] Shutting down gracefully")
